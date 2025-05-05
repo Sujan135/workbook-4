894f524147959433d65313b0726b06823edeef16
Sujan Banjara <banjara.suzan12@gmail.com>
2025-05-05 13:21:22 -0500

    Created HotelOperations project and implemented Room class

---
+++ HotelOperations/src/main/java/com/pluralsight/Room.java
@@ -0,0 +1,39 @@
+package com.pluralsight;
+
+public class Room {
+    private final int numberOfBeds;
+    private final double price;
+    private final boolean occupied;
+    private final boolean dirty;
+
+    public Room(int numberOfBeds, double price, boolean occupied, boolean dirty) {
+        this.numberOfBeds = numberOfBeds;
+        this.price = price;
+        this.occupied = occupied;
+        this.dirty = dirty;
+    }
+
+    public int getNumberOfBeds() {
+        return numberOfBeds;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public boolean isOccupied() {
+        return occupied;
+    }
+
+    public boolean isDirty() {
+        return dirty;
+    }
+
+    public boolean isAvailable() {
+        if (!this.occupied && !this.dirty) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
