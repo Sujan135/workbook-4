355fc24b449eeeea812434f30053119d78f19ffe
Sujan Banjara <banjara.suzan12@gmail.com>
2025-05-05 13:39:27 -0500

    Added Reservation class with room type, weekend rate logic, and total calculation

---
+++ HotelOperations/src/main/java/com/pluralsight/Reservation.java
@@ -0,0 +1,51 @@
+package com.pluralsight;
+
+public class Reservation {
+    private String roomType;
+    private int numberOfNights;
+    private boolean isWeekend;
+
+    public Reservation(String roomType, int numberOfNights, boolean isWeekend) {
+        this.roomType = roomType;
+        this.numberOfNights = numberOfNights;
+        this.isWeekend = isWeekend;
+    }
+
+    public String getRoomType() {
+        return roomType;
+    }
+
+    public void setRoomType(String roomType) {
+        this.roomType = roomType;
+    }
+
+    public int getNumberOfNights() {
+        return numberOfNights;
+    }
+
+    public void setNumberOfNights(int numberOfNights) {
+        this.numberOfNights = numberOfNights;
+    }
+
+    public boolean isWeekend() {
+        return isWeekend;
+    }
+
+    public void setWeekend(boolean weekend) {
+        isWeekend = weekend;
+    }
+
+    public double getPrice() {
+        double price = roomType.equalsIgnoreCase("king") ? 139.0 : 124.0;
+        if (isWeekend) {
+            price *= 1.10;
+        }
+        return price;
+
+    }
+
+    public double getReservationTotal() {
+        return getPrice() * this.numberOfNights;
+    }
+
+}
