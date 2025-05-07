e12b82bfe43adafc32b8af9f79faecffe5e61564
Sujan Banjara <banjara.suzan12@gmail.com>
2025-05-07 14:08:29 -0500

    Add test for checkIn method to set room as occupied and dirty

---
+++ HotelOperations/src/test/java/com/pluralsight/RoomTest.java
@@ -0,0 +1,23 @@
+package com.pluralsight;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RoomTest {
+
+    @Test
+    void checkIn_SetOccupiedAndDirty() {
+
+        // arrange
+        Room room = new Room(2, 100.0);
+
+        // act
+        room.checkIn();
+
+        // assert
+        assertTrue(room.isOccupied());
+        assertTrue(room.isDirty());
+    }
+
+}
