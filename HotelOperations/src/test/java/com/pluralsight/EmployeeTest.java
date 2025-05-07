c9e33f20c8c2f52dc7e1a159acc332cf577aa617
Sujan Banjara <banjara.suzan12@gmail.com>
2025-05-07 14:59:29 -0500

    Implement punchOut test to verify correct total hours calculation.

--- HotelOperations/src/test/java/com/pluralsight/EmployeeTest.java
+++ HotelOperations/src/test/java/com/pluralsight/EmployeeTest.java
@@ -21,6 +21,21 @@ class EmployeeTest {
     }
 
     @Test
-    void punchOut() {
+    void punchOut_CalculateTotalHours() {
+
+        // arrange
+        Employee employee = new Employee(1, "Sujan Banjara", "Front Office", 20.0, 0);
+        employee.punchTimeCard(9.0);
+
+        // act
+        employee.punchTimeCard(17.0);
+
+        // assert
+
+        double expectedTotalHours = 8.0;
+
+        double actual = employee.getHoursWorked();
+        assertEquals(expectedTotalHours, actual);
+
     }
 }
