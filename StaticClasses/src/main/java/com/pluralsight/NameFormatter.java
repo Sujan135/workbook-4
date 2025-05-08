34a7d835262f9a11ea625041429db2003386c93f
Sujan Banjara <banjara.suzan12@gmail.com>
2025-05-08 09:30:25 -0500

    Added second format method for full name with prefix, middle name, and suffix.

--- StaticClasses/src/main/java/com/pluralsight/NameFormatter.java
+++ StaticClasses/src/main/java/com/pluralsight/NameFormatter.java
@@ -5,7 +5,11 @@ public class NameFormatter {
 
 
     public static String format(String firstName, String lastname){
-        return lastname + "," + firstName;
-
+        return lastname + ", " + firstName;
     }
+
+    public static String format(String prefix, String lastName, String firstName, String middleName,String suffix) {
+        return lastName + ", " + prefix + " " + firstName + " " + middleName + ", " + suffix;
+    }
+
 }
