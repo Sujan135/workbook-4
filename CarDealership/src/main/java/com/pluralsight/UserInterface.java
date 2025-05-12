2e6bbeaca9f2ea5f658e0e4c4fbeb66f201120d8
Sujan Banjara <banjara.suzan12@gmail.com>
2025-05-12 14:47:32 -0500

    Created user interface to initialize dealership, display vehicles, and handle user input for viewing all vehicles

--- CarDealership/src/main/java/com/pluralsight/UserInterface.java
+++ CarDealership/src/main/java/com/pluralsight/UserInterface.java
@@ -1,21 +1,23 @@
 package com.pluralsight;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class UserInterface {
     private Dealership dealership;
 
     public void display() {
-        DealershipFileManager fileManager = new DealershipFileManager();
-        dealership = fileManager.getDealerShip();
+        init();
 
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
+            displayMenu();
             int command = scanner.nextInt();
 
             switch (command) {
                 case 1:
+                    processAllVehiclesRequest();
                     break;
                 case 2:
                     break;
@@ -32,6 +34,11 @@ public class UserInterface {
         }
     }
 
+    private void init() {
+        DealershipFileManager fileManager = new DealershipFileManager();
+        dealership = fileManager.getDealerShip();
+    }
+
 
     private void displayMenu() {
         System.out.println("-- Dealership Menu --");
@@ -40,5 +47,26 @@ public class UserInterface {
         System.out.println("3. View vehicles by make/model");
         System.out.println("4. Exit");
         System.out.println(" Enter your command");
+    }
+
+    private void displayVehicles(List<Vehicle> vehicles){
+        if (vehicles.isEmpty()) {
+            System.out.println("No vehicles found.");
+        }else {
+            for (Vehicle vehicle : vehicles) {
+                System.out.println(vehicle.getYear()
+                        + " " + vehicle.getMake() +
+                        " " + vehicle.getModel()
+                        + " - $" + vehicle.getPrice());
+            }
+        }
+    }
+
+    private void processAllVehiclesRequest() {
+        if (dealership != null) {
+            displayVehicles(dealership.getAllVehicles());
+        }else {
+            System.out.println("Dealership not found.");
+        }
     }
 }
